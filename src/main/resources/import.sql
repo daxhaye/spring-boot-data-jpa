4633b0e013f1b5746c062cada13334d06a54d267
David Haye <davidhaye@hotmail.com>
2021-03-26 16:31:52 -0300

    Proyecto JPA - curso

---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+/* Populate tables */
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES(1, 'David', 'Haye', 'davidhaye@hotmail.com', '2021-03-24');
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES(2, 'Marina', 'Vaque', 'maru_vaque@hotmail.com', '2021-03-24');
