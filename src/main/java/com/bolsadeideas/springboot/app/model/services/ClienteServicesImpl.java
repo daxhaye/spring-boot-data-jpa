2a9cea473604927b3e896602bbf940394f1dc886
David Haye <davidhaye@hotmail.com>
2021-03-27 16:41:02 -0300

    Layout para sacar código repetido en las vistas - Pie de página - Imagenes - y mensajes personalizados para los clientes.

---
+++ src/main/java/com/bolsadeideas/springboot/app/model/services/ClienteServicesImpl.java
@@ -0,0 +1,43 @@
+package com.bolsadeideas.springboot.app.model.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.bolsadeideas.springboot.app.model.dao.IClienteDao;
+import com.bolsadeideas.springboot.app.model.entity.Cliente;
+
+@Service
+public class ClienteServicesImpl implements IClienteServices {
+
+	@Autowired
+	private IClienteDao clienteDao;
+	
+	@Override
+	@Transactional(readOnly=true)
+	public List<Cliente> findAll() {
+		return (List<Cliente>) clienteDao.findAll();
+	}
+
+	@Override
+	@Transactional
+	public void save(Cliente cliente) {
+		clienteDao.save(cliente);
+	}
+
+	@Override
+	@Transactional(readOnly = true)
+	public Cliente findOne(Long id) {
+		return clienteDao.findById(id).orElse(null);
+	}
+
+	@Override
+	@Transactional
+	public void delete(Long id) {
+		clienteDao.deleteById(id);
+	}
+	
+	
+}
